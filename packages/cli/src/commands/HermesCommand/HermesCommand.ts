046c6b0f32d9989c39d2d390a6256ae302a1147e
MJ <mym0404@gmail.com>
2026-06-26 18:11:55 +0900

    fix: simplify hermes sourcemap option

--- packages/cli/src/commands/HermesCommand/HermesCommand.ts
+++ packages/cli/src/commands/HermesCommand/HermesCommand.ts
@@ -9,7 +9,6 @@ export function hermesCommand() {
   return new Command('hermes')
     .description('지정한 번들을 Hermes 바이트 코드로 컴파일 합니다')
     .requiredOption('--jsbundle <path>', 'Hermes 바이트 코드로 컴파일 할 Javascript 파일 경로 입니다')
-    .option('--sourcemap', '소스맵 파일을 생성합니다', true)
     .option('--no-sourcemap', '소스맵 파일을 생성하지 않습니다')
     .action(async ({ jsbundle, sourcemap }) => {
       process.exitCode = await executeHermes({ jsBundleFile: jsbundle, sourcemap });
